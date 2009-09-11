2f4d46d9a1316e2ef5924372717dde415344ae6c
Ken <dengjing.cn@gmail.com>
2009-09-11 15:46:24 +0800

    new file:   dwm-5.6.1/Already-patched 	new file:   dwm-5.6.1/LICENSE 	new file:   dwm-5.6.1/Makefile 	new file:   dwm-5.6.1/README 	new file:   dwm-5.6.1/attachabove-5.6.1.diff 	new file:   dwm-5.6.1/bstack.c 	renamed:    dwm-5.6.1/config.h -> dwm-5.6.1/config.def.h 	new file:   dwm-5.6.1/config.mk 	new file:   dwm-5.6.1/dwm-5.6.1-bstack.diff 	new file:   dwm-5.6.1/dwm-5.6.1-fibonacci.diff 	new file:   dwm-5.6.1/dwm.1 	new file:   dwm-5.6.1/dwm.c 	new file:   dwm-5.6.1/fibonacci.c 	new file:   dwm-5.6.1/gaplessgrid.c 	new file:   dwm-5.6.1/nmaster.c 	new file:   dwm-5.6.1/push.c

--- dwm-5.6.1/config.def.h
+++ dwm-5.6.1/config.def.h
@@ -1,51 +1,65 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-microsoft yahei-medium-*-*-*-11-*-*-*-*-*-*-*";
-static const char normbordercolor[] = "#444444";
+static const char font[]            = "-*-microsoft yahei-medium-r-normal-*-11-*-*-*-*-*-*-*";
+static const char normbordercolor[] = "#3f3f3f";
 static const char normbgcolor[]     = "#101010";
 static const char normfgcolor[]     = "#777777";
-static const char selbordercolor[]  = "#1a73b1";
-static const char selbgcolor[]      = "#1a73b1";
-static const char selfgcolor[]      = "#efefef";
+static const char selbordercolor[]  = "#f0dfaf";
+static const char selbgcolor[]      = "#000000";
+static const char selfgcolor[]      = "#f0dfaf";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
+static const int nmaster = 2;  /* default number of clients in the master area */
 
 /* tagging */
 static const char *tags[] = { "main", "web", "code", "im", "media" };
 
 static const Rule rules[] = {
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 2,       True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       False,       -1 },
-	{ "trayer",   NULL,       NULL,       1 << 3,       False,       -1 },
-	{ "Thunar",   NULL,       NULL,       0,            True,        -1 },
-	{ "Pidgin",   NULL,       NULL,       0,            True,        -1 },
-	{ "MPlayer",        NULL,       NULL,       0,              True, -1  },
-	{ "Sonata",         NULL,       NULL,       0,              True, -1  },
-	{ "Qq",             NULL,       NULL,       0,              True, -1  },
-	{ "SMPlayer",       NULL,       NULL,       0,              True, -1  },
-	{ "Gtk-chtheme",    NULL,       NULL,       0,              True, -1  },
-	{ "File-roller",    NULL,       NULL,       0,              True, -1  },
-	{ "XFontSel",       NULL,       NULL,       0,              True, -1  },
-	{  NULL,            NULL, "File Management Preferences", 0, True, -1  },
-	{  NULL,          "file_properties",  NULL, 0,              True, -1  },
-
+	/* class          instance      title       tags mask     isfloating   monitor */
+	{ "Gimp",           NULL,       NULL,       1 << 2,         True,        -1 },
+	{ "Firefox",        NULL,       NULL,       1 << 1,         False,       -1 },
+	{ "trayer",         NULL,       NULL,       1 << 3,         False,       -1 },
+	{ "Thunar",         NULL,       NULL,       0,              True,        -1 },
+	{ "Pidgin",         NULL,       NULL,       1 << 3,         True,        -1 },
+	{ "Skype",          NULL,       NULL,       1 << 3,         True,        -1 },
+        { "Stardict",       NULL,       NULL,       0,              True,        -1 },
+        { "Gpicview",       NULL,       NULL,       0,              True,        -1 },
+        { "Gmlive",         NULL,       NULL,       0,              True,        -1 },
+        { "Eog",            NULL,       NULL,       0,              True,        -1 },
+	{ "MPlayer",        NULL,       NULL,       0,              True,        -1 },
+	{ "Sonata",         NULL,       NULL,       0,              True,        -1 },
+	{ "Qq",             NULL,       NULL,       0,              True,        -1 },
+	{ "SMPlayer",       NULL,       NULL,       0,              True,        -1 },
+	{ "Gtk-chtheme",    NULL,       NULL,       0,              True,        -1 },
+	{ "File-roller",    NULL,       NULL,       0,              True,        -1 },
+	{ "XFontSel",       NULL,       NULL,       0,              True,        -1 },
+	{  NULL,            NULL, "File Management Preferences", 0, True,        -1 },
+	{  NULL,          "file_properties",  NULL, 0,              True,        -1 },
 };
 
 /* layout(s) */
 static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const Bool resizehints = True; /* False means respect size hints in tiled resizals */
+static const Bool resizehints = False; /* False means respect size hints in tiled resizals */
 
 #include "bstack.c"
+#include "gaplessgrid.c"
+#include "nmaster.c"
+#include "fibonacci.c"
+#include "push.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "TTT",      bstack },
+	{ "###",      gaplessgrid },
+        { "-|=",      ntile },
+        { "-|-",      nbstack },
+        { "[@]",      spiral },
+        { "[\\]",     dwindle },
 };
 
 /* key definitions */
@@ -62,6 +76,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
+static const char *rxvtcmd[]  = { "urxvt", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 static const char *filecmd[]  = { "thunar", NULL };
 static const char *commandcmd[]  = { "gmrun", NULL };
@@ -69,32 +84,44 @@ static const char *musicmd[]  = { "mpc", "next", NULL };
 static const char *mutecmd[] = { "amixer", "-q", "sset", "Master", "toggle", NULL };
 static const char *volupcmd[] = { "amixer", "-q", "sset", "Master", "2+", "unmute", NULL };
 static const char *voldowncmd[] = { "amixer", "-q", "sset", "Master", "2-", "unmute", NULL };
+static const char *snapshotcmd[] = { "scrot", "-d", "3", "snapshot-%Y%m%d-%H.%M.%S.png",NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_Print,  spawn,          {.v = snapshotcmd } },
 	{ MODKEY,                       XK_F1,     spawn,          {.v = commandcmd } },
-	{ MODKEY,                       XK_F2,     spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_Up,     spawn,          {.v = volupcmd } },
+        { MODKEY,                       XK_F2,     spawn,          {.v = dmenucmd } },
+        { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+        { MODKEY|ControlMask,           XK_Return, spawn,          {.v = rxvtcmd } },
+	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = filecmd } },
+        { MODKEY|ShiftMask,             XK_Up,     spawn,          {.v = volupcmd } },
 	{ MODKEY|ShiftMask,             XK_Down,   spawn,          {.v = voldowncmd } },
 	{ MODKEY|ShiftMask,             XK_Left,   spawn,          {.v = mutecmd } },
 	{ MODKEY|ShiftMask,             XK_Right,  spawn,          {.v = musicmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },            /* classic alt-tab */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+        { MODKEY|ControlMask,           XK_j,      pushdown,       {0} },
+        { MODKEY|ControlMask,           XK_k,      pushup,         {0} },
+	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_z,      view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = browsercmd } },
-	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = filecmd } },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_q,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+        { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[4]} },
+        { MODKEY,                       XK_w,      setlayout,      {.v = &layouts[5] } },
+        { MODKEY,                       XK_e,      setlayout,      {.v = &layouts[6] } },
+        { MODKEY,                       XK_s,      setlayout,      {.v = &layouts[7]} },
+        { MODKEY,                       XK_c,      setlayout,      {.v = &layouts[8]} },
+        { MODKEY,                       XK_a,      incnmaster,     {.i = +1} },
+        { MODKEY,                       XK_z,      incnmaster,     {.i = -1} },
+        { MODKEY,                       XK_x,      setnmaster,     {.i = 2} },
+        { MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
